adf30915cf0a6d1223b6c4247d6819b5611c2ccc
dm1trygr <103391790+dm1trygr@users.noreply.github.com>
2023-08-23 14:18:53 +0300

    Added support of unit tests
    - Added unit tests mode (-t flag to do tests)

--- main.cpp
+++ main.cpp
@@ -1,23 +1,33 @@
 #include <stdio.h>
 #include <math.h>
 #include <float.h>
+#include <string.h>
 #include "square_solver.h"
 
-int main(void) {
-    printf("Welcome to Square equation solver\n"
-           "This program can solve square equations\n\n");
+int main(int argc, char * argv[]) {
+    if (argc >= 2 && strcmp(argv[1], "-t") == 0) {
+        printf("Square equation solver, Unit tests mode\n\n");
 
-    double a = 0, b = 0, c = 0;
+        runtests();
 
-    get_3_coeffs(&a, &b, &c);
+        return 0;
+    }
+    else {
+        printf("Welcome to Square equation solver\n"
+               "This program can solve square equations\n\n");
 
-    double x1 = 0, x2 = 0;
-    int roots_amount = solve(a, b, c, &x1, &x2);
+        double a = 0, b = 0, c = 0;
 
-    print_solutions(roots_amount, x1, x2);
+        get_3_coeffs(&a, &b, &c);
 
-    printf("Press any key to exit from program\n");
-    getchar();
+        double x1 = 0, x2 = 0;
+        int roots_amount = solve(a, b, c, &x1, &x2);
 
-    return 0;
+        print_solutions(roots_amount, x1, x2);
+
+        printf("Press any key to exit from program\n");
+        getchar();
+
+        return 0;
+    }
 }
