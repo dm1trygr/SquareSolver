5ccb767d95c0ea49c8904b035c5cd027bd3bb88a
dm1trygr <103391790+dm1trygr@users.noreply.github.com>
2023-08-24 21:55:07 +0300

    Added string constants for flags

--- main.cpp
+++ main.cpp
@@ -6,14 +6,20 @@
 
 int main(int argc, char * argv[]) { // добавить режим чтени€ из файла в основном режиме
     if (argc >= 2) {
-        if (strcmp(argv[1], "-t") == 0) {
-            unit_tests_mode();
+        if (strcmp(argv[1], SQUARE_MODE_FLAG) == 0) {
+            square_equation_mode();
         }
-        else if (strcmp(argv[1], "-l") == 0) {
+        else if (strcmp(argv[1], LINEAR_MODE_FLAG) == 0) {
             linear_equation_mode();
         }
-        else if (strcmp(argv[1], "-s") == 0) {
-            square_equation_mode();
+        else if (strcmp(argv[1], FILE_MODE_FLAG) == 0) {
+            file_mode();
+        }
+        else if (strcmp(argv[1], UNIT_TESTS_MODE) == 0) {
+            unit_tests_mode();
+        }
+        else if (strcmp(argv[1], FULL_HELP_MODE) == 0) {
+            full_help(argv[0]);
         }
         else {
             short_help(argv[0]);
--- modes.cpp
+++ modes.cpp
@@ -4,27 +4,6 @@
 #include "solve.h"
 #include "unittests.h"
 
-void unit_tests_mode(void) {
-    printf("Square equation solver, Unit tests mode\n\n");
-
-    run_tests();
-}
-
-void linear_equation_mode(void) {
-    printf("Square equation solver, linear equation solver mode\n\n");
-
-    double coeffs[2] = {0, 0};
-
-    do {
-        get_coeffs(coeffs, LINEAR_COEFFS_AMOUNT);  // в функцию
-        double roots[1] = {0};
-        int roots_amount = solve_linear(coeffs[0], coeffs[1], &roots[0]);
-        print_solutions(roots, roots_amount);
-    } while (continue_program() == 1);
-
-    printf("Goodbye!");
-}
-
 void square_equation_mode(void) {
     printf("Welcome to Square equation solver\n"
            "This program can solve square equations\n\n");
@@ -43,11 +22,45 @@ void square_equation_mode(void) {
     printf("Goodbye!");
 }
 
+void linear_equation_mode(void) {
+    printf("Square equation solver, linear equation solver mode\n\n");
+
+    double coeffs[2] = {0, 0};
+
+    do {
+        get_coeffs(coeffs, LINEAR_COEFFS_AMOUNT);  // в функцию
+        double roots[1] = {0};
+        int roots_amount = solve_linear(coeffs[0], coeffs[1], &roots[0]);
+        print_solutions(roots, roots_amount);
+    } while (continue_program() == 1);
+
+    printf("Goodbye!");
+}
+
+void file_mode(void) {
+
+}
+
+void unit_tests_mode(void) {
+    printf("Square equation solver, Unit tests mode\n\n");
+
+    run_tests();
+}
+
+void full_help(const char * const program_name) {
+
+}
+
 void short_help(const char * const program_name) {
-    printf("Usage: %s -s for square solver mode;\n"  // -h для вывода полной инфы о квадратке
-           "       %s -l for linear mode;\n"
-           "       %s -t for unit tests mode;\n"
-           "       %s -h to show full information",
-           program_name, program_name, program_name, program_name);
+    printf("Usage: %s %s for square solver mode;\n"  // -h для вывода полной инфы о квадратке
+           "       %s %s for linear mode;\n"
+           "       %s %s for file input/output mode;\n"
+           "       %s %s for unit tests mode;\n"
+           "       %s %s to show full information",
+           program_name, SQUARE_MODE_FLAG,
+           program_name, LINEAR_MODE_FLAG,
+           program_name, FILE_MODE_FLAG,
+           program_name, UNIT_TESTS_MODE,
+           program_name, FULL_HELP_MODE);
 }
 
